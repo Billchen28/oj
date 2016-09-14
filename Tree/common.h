95d10fc81f880147de059519c2285bdd08b43a83
Bill <bill@floatyun.com>
2016-09-14 08:28:20 +0800

    support mac

--- Tree/common.h
+++ Tree/common.h
@@ -8,7 +8,20 @@
 #ifndef COMMON_H_
 #define COMMON_H_
 
+//#include <malloc.h>
+#include <sys/malloc.h>
+
+#ifdef _WIN32 // note the underscore: without it, it's not msdn official! 
+#include <malloc.h> 
+#elif __unix__ // all unices 
 #include <malloc.h>
+#elif __posix__ 
+#include <malloc.h>
+#elif __linux__ 
+#include <malloc.h>
+#elif __APPLE__ 
+#include <sys/malloc.h>
+#endif
 
 #ifndef NULL
 #define NULL 0
